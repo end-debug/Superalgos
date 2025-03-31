d59a6b537c136567f340e5fb4d2e5c1396b18369
End-debug <65282771+end-debug@users.noreply.github.com>
2025-03-31 16:18:00 +0200

    Update index.js
    Removed routes, mistake

--- Social-Trading/Vue-UI/src/router/index.js
+++ Social-Trading/Vue-UI/src/router/index.js
@@ -5,62 +5,9 @@ import SocialTradingView from '../views/SocialTradingView.vue'
 
 
 const routes = [
-  
-    path: '/',
+     {
+    path: '/howe',
     name: 'Home',
-    component: MainAppView,
-    meta: { hasExtraColumns: true, useDrawerOnMobile: true}
-  },
-  {
-    path: '/Charts',
-    name: 'charts',
-    component: ChartsView,
-    meta: { hasExtraColumns: false, useDrawerOnMobile: true}
-  },
-  {
-    path: '/Dashboard',
-    name: 'dashboard',
-    component: DashboardView,
-    meta: { hasExtraColumns: false, useDrawerOnMobile: true}
-  },{
-    path: '/Followers',
-    name: 'followers',
-    component: FollowersView,
-    meta: { hasExtraColumns: false, useDrawerOnMobile: true}
-  },
-  {
-    path: '/portfolio',
-    name: 'portfolio',
-    component: PortfolioView,
-    meta: { hasExtraColumns: true, useDrawerOnMobile: true}
-  },
-  
-  {
-    path: '/Networks',
-    name: 'networks',
-    component: NetworksView,
-    meta: { hasExtraColumns: false, useDrawerOnMobile: true}
-  },
-  {
-    path: '/Bots',
-    name: 'bots',
-    component: BotsView
-  },
-  {
-    path: '/Users',
-    name: 'users',
-    component: UsersView,
-    meta: { hasExtraColumns: false, useDrawerOnMobile: true}
-  },
-  {
-    path: '/Todos',
-    name: 'todos',
-    component: TodosView,
-    meta: { hasExtraColumns: false, useDrawerOnMobile: true}
-  },
-  {
-    path: '/version-1',
-    name: 'vs-1',
     component: SocialTradingView
   }
 ]
